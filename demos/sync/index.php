9d0dbbcd1a14fa1e2c56efd7ef4977fccea539e4
Micky Hulse <mickyhulse@gmail.com>
2014-08-22 12:18:47 -0700

    Added query cookie

--- demos/sync/index.php
+++ demos/sync/index.php
@@ -117,6 +117,7 @@
 	</div> <!-- /.wiffle -->
 	
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script src="../assets/scripts/jquery.cookie.min.js"></script>
 	
 	<script src="http://advertising.registerguard.com/assets/billboard/1.4/js/billboard.js"></script>
 	
