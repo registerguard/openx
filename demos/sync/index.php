19452bb06a4d6cd73c068813d926b359dbf856c2
Micky Hulse <mickyhulse@gmail.com>
2014-08-21 18:35:45 -0700

    Style clean up.

--- demos/sync/index.php
+++ demos/sync/index.php
@@ -28,6 +28,7 @@
 		_ox.addAdUnit('328209');    // Leaderboard Bottom
 		_ox.addAdUnit('537187629'); // Mobile Banner 1 - Bottom
 		<?php parse_str($_SERVER['QUERY_STRING'], $topics); ?>
+		// Content Topics:
 		<?php foreach ($topics as $key => $value): ?>_ox.addContentTopic(<?=$key?>, '<?=$value?>');<?php endforeach; ?>
 		_ox.fetchAds();
 	</script>
@@ -44,11 +45,11 @@
 		<hr>
 		
 		<!-- //////////////////// Leaderboard 1 \\\\\\\\\\\\\\\\\\\\ -->
-		<div class="leaderboard advert"><script>_ox.showAdUnit('537175626');</script></div>
+		<div class="leaderboard advert"><div id="leaderboard-1"><script>_ox.showAdUnit('537175626');</script></div></div>
 		<!-- \\\\\\\\\\\\\\\\\\\\ Leaderboard 1 //////////////////// -->
 		
 		<!-- //////////////////// Mobile Banner 1 \\\\\\\\\\\\\\\\\\\\ -->
-		<div class="leaderboard advert"><script>_ox.showAdUnit('537175856');</script></div>
+		<div class="mobile-banner advert"><div id="mobile-banner-1"><script>_ox.showAdUnit('537175856');</script></div></div>
 		<!-- \\\\\\\\\\\\\\\\\\\\ Mobile Banner 1 //////////////////// -->
 		
 		<hr>
@@ -78,25 +79,25 @@
 			<div class="w_col w_B300 Blb Clb Dlb">
 				
 				<!-- //////////////////// Medium Rectangle 1 \\\\\\\\\\\\\\\\\\\\ -->
-				<div class="medium_rectangle advert"><script>_ox.showAdUnit('537175627');</script></div>
+				<div class="medium-rectangle advert"><div id="medium-rectangle-1"><script>_ox.showAdUnit('537175627');</script></div></div>
 				<!-- \\\\\\\\\\\\\\\\\\\\ Medium Rectangle 1 //////////////////// -->
 				
 				<hr>
 				
 				<!-- //////////////////// Half Page 1 \\\\\\\\\\\\\\\\\\\\ -->
-				<div class="medium_rectangle advert"><script>_ox.showAdUnit('537175638');</script></div>
+				<div class="half-page advert"><div id="half-page-1"><script>_ox.showAdUnit('537175638');</script></div></div>
 				<!-- \\\\\\\\\\\\\\\\\\\\ Half Page 1 //////////////////// -->
 				
 				<hr>
 				
 				<!-- //////////////////// Homepage Block 1 \\\\\\\\\\\\\\\\\\\\ -->
-				<div class="column_block advert"><script>_ox.showAdUnit('537175631');</script></div>
+				<div class="column-block advert"><div id="homepage-block-1"><script>_ox.showAdUnit('537175631');</script></div></div>
 				<!-- \\\\\\\\\\\\\\\\\\\\ Homepage Block 1 //////////////////// -->
 				
 				<hr>
 				
 				<!-- //////////////////// Medium Rectangle 2 \\\\\\\\\\\\\\\\\\\\ -->
-				<div class="medium_rectangle advert"><script>_ox.showAdUnit('537175629');</script></div>
+				<div class="medium-rectangle advert"><div id="medium-rectangle-2"><script>_ox.showAdUnit('537175629');</script></div></div>
 				<!-- \\\\\\\\\\\\\\\\\\\\ Medium Rectangle 2 //////////////////// -->
 				
 			</div> <!-- /.w_col -->
@@ -106,11 +107,11 @@
 		<hr>
 		
 		<!-- //////////////////// Leaderboard Bottom \\\\\\\\\\\\\\\\\\\\ -->
-		<div class="leaderboard advert"><script>_ox.showAdUnit('328209');</script></div>
+		<div class="leaderboard advert"><div id="leaderboard-bottom"><script>_ox.showAdUnit('328209');</script></div></div>
 		<!-- \\\\\\\\\\\\\\\\\\\\ Leaderboard Bottom //////////////////// -->
 		
 		<!-- //////////////////// Mobile Banner 1 - Bottom \\\\\\\\\\\\\\\\\\\\ -->
-		<div class="leaderboard advert"><script>_ox.showAdUnit('537187629');</script></div>
+		<div class="mobile-banner advert"><div id="mobile-banner-1-bottom"><script>_ox.showAdUnit('537187629');</script></div></div>
 		<!-- \\\\\\\\\\\\\\\\\\\\ Mobile Banner 1 - Bottom //////////////////// -->
 		
 	</div> <!-- /.wiffle -->
