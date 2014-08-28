3f9c4f6ae9c57524e971dcaf24859dc38cc75531
Micky Hulse <mickyhulse@gmail.com>
2014-08-28 11:59:48 -0700

    Page type navigation.

--- demos/sync/includes/meta.php
+++ demos/sync/includes/meta.php
@@ -10,6 +10,10 @@
 	<b><a href="https://docs.google.com/spreadsheets/d/1Qa0ambORhI9jYjIdGD79SjLkCQggg89MEtjR6liIm8g" target="_blank">Content Topics</a>:</b> <?php foreach ($topics as $key => $value): ?><?=$key?>=<?=$value?> <? endforeach; ?>
 </p>
 
-<p><b>Presets:</b> <a href="?3=business&amp;3001=local&amp;3002=blue%20chip">Business</a> | <a href="?6=entertainment">Entertainment</a></p>
+<p>
+	<b>Presets:</b> <a href="?3=business&amp;3001=local&amp;3002=blue%20chip">Business</a> | <a href="?6=entertainment">Entertainment</a>
+	<br>
+	<b>Page types:</b> <a href="./">Section Front</a> | <a href="story.php">Story Page</a>
+</p>
 
 <p><b>Note:</b> Although the content topics appear hierarchical; targeting a top-level content topic does not target its sub-topics. For example, targeting the top-level content topic, Automotive, does not target its sub-topics, Automotive: Trucks or Automotive: Cars (<a href="http://docs.openx.com/ad_server/#targeting_doc_contenttopic.html">source</a>).</p>
